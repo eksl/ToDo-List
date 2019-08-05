c7409420039e28ebcabd997526384091eac74a1b
eksl <willy.eksel@gmail.com>
2019-08-05 19:42:17 +0200

    Add elements

--- js/app.js
+++ js/app.js
@@ -14,25 +14,31 @@ function addNewList() {
     // Create list
     var newDiv = document.createElement("div");
     newDiv.classList.add("todo-container");
+
     var newTitle = document.createElement("input");
     newTitle.value = "New List";
     newTitle.classList.add("todo-container__title");
+
     var addButton = document.createElement("button");
     addButton.innerText = "+";
     addButton.classList.add("todo-container__add-element");
     addButton.addEventListener("click", addListElement);
-    var removeButton = document.createElement("button");
-    removeButton.innerText = "x";
-    removeButton.classList.add("todo-container__remove-list");
+
+    var removeListButton = document.createElement("button");
+    removeListButton.innerText = "x";
+    removeListButton.classList.add("todo-container__remove-list");
+    removeListButton.addEventListener("click", removeList);
+
     var removeCompletedButton = document.createElement("button");
     removeCompletedButton.innerText = "Remove completed";
     removeCompletedButton.classList.add("todo-container__remove-completed");
+
     var newList = document.createElement("ul");
     newList.classList.add("todo-container__list");
 
     newDiv.appendChild(newTitle);
     newDiv.appendChild(addButton);
-    newDiv.appendChild(removeButton);
+    newDiv.appendChild(removeListButton);
     newDiv.appendChild(removeCompletedButton);
     newDiv.appendChild(newList);
 
@@ -47,4 +53,13 @@ function handleAddListButton() {
 // Add list element
 function addListElement() {
     console.log(this);
+    var element = document.createElement("li");
+    element.classList.add("todo-container__list-element");
+    this.parentElement.querySelector(".todo-container__list").appendChild(element);
+}
+
+// Remove list
+function removeList() {
+    var thisDiv = this.parentElement;
+    thisDiv.parentElement.removeChild(thisDiv);
 }
