bbb82de30132a8cc4caba4fdd3e9000bc3468b30
eksl <willy.eksel@gmail.com>
2019-08-06 17:02:14 +0200

    Moving lists with mouse

--- js/app.js
+++ js/app.js
@@ -9,14 +9,14 @@ function main() {
 
 // Handle adding new lists
 function addNewList() {
-    var container = document.querySelector(".main-container .main-width");
+    var container = document.querySelector(".main-container");
 
     // Create list
     var newDiv = document.createElement("div");
     newDiv.classList.add("todo-container");
 
     var newTitle = document.createElement("input");
-    newTitle.value = "New List";
+    newTitle.value = "New list";
     newTitle.classList.add("todo-container__title");
 
     var addButton = document.createElement("button");
@@ -43,6 +43,41 @@ function addNewList() {
     newDiv.appendChild(newList);
 
     container.appendChild(newDiv);
+
+    // Events
+
+    var mousePosition;
+    var offset = [0, 0];
+    var div;
+    var isDown = false;
+    newDiv.style.left = "0px";
+    newDiv.style.top = "0px";
+
+    newDiv.addEventListener('mousedown', function (e) {
+        isDown = true;
+        offset = [
+            newDiv.offsetLeft - e.clientX,
+            newDiv.offsetTop - e.clientY
+        ];
+    }, true);
+
+    document.addEventListener('mouseup', function () {
+        isDown = false;
+    }, true);
+
+    document.addEventListener('mousemove', function (event) {
+        event.preventDefault();
+        if (isDown) {
+            mousePosition = {
+
+                x: event.clientX,
+                y: event.clientY
+
+            };
+            newDiv.style.left = (mousePosition.x + offset[0]) + 'px';
+            newDiv.style.top = (mousePosition.y + offset[1]) + 'px';
+        }
+    }, true);
 }
 
 function handleAddListButton() {
@@ -55,6 +90,26 @@ function addListElement() {
     console.log(this);
     var element = document.createElement("li");
     element.classList.add("todo-container__list-element");
+
+    // Input
+    var elementInput = document.createElement("input");
+    elementInput.classList.add("todo-container__list-element-input");
+    elementInput.value = "New element";
+    element.appendChild(elementInput);
+
+    // Complete button
+    var completeButton = document.createElement("button");
+    completeButton.classList.add("todo-container__list-element-complete");
+    completeButton.innerText = "V";
+    element.appendChild(completeButton);
+
+    // Remove button
+    var removeButton = document.createElement("button");
+    removeButton.classList.add("todo-container__list-element-remove");
+    removeButton.innerText = "x";
+    element.appendChild(removeButton);
+
+    // Add element to document
     this.parentElement.querySelector(".todo-container__list").appendChild(element);
 }
 
@@ -62,4 +117,9 @@ function addListElement() {
 function removeList() {
     var thisDiv = this.parentElement;
     thisDiv.parentElement.removeChild(thisDiv);
+}
+
+// Move list with mouse
+function moveList() {
+
 }
